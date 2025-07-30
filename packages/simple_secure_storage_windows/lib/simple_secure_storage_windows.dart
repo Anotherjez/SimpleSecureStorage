2d550bfe5bfaf6193dcde8544183dc1245e4cc58
Jesus <anotherjez2@gmail.com>
2025-07-29 21:21:30 -0400

    Export platform interface on Windows

--- packages/simple_secure_storage_windows/lib/simple_secure_storage_windows.dart
+++ packages/simple_secure_storage_windows/lib/simple_secure_storage_windows.dart
@@ -1,4 +1,6 @@
 import 'package:simple_secure_storage_platform_interface/simple_secure_storage_platform_interface.dart';
 
+export 'package:simple_secure_storage_platform_interface/src/initialization_options.dart';
+
 /// Windows implementation of SimpleSecureStorage.
 class SimpleSecureStorageWindows extends MethodChannelSimpleSecureStorage {}

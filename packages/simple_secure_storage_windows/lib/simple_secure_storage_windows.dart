1ef1706056be61de62e9d64e67902c93dfd19a18
Jesus <anotherjez2@gmail.com>
2025-07-29 21:56:22 -0400

    Possible fix for Windows build

--- packages/simple_secure_storage_windows/lib/simple_secure_storage_windows.dart
+++ packages/simple_secure_storage_windows/lib/simple_secure_storage_windows.dart
@@ -1,6 +1,4 @@
 import 'package:simple_secure_storage_platform_interface/simple_secure_storage_platform_interface.dart';
 
-export 'package:simple_secure_storage_platform_interface/src/initialization_options.dart';
-
 /// Windows implementation of SimpleSecureStorage.
 class SimpleSecureStorageWindows extends MethodChannelSimpleSecureStorage {}
